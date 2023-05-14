8042a287326be73f85b2cacc2c79ae6ebafe9b3a
Marlene Inoue <mminoue@ucsc.edu>
2023-05-14 14:04:54 -0700

    added penguin ending screen

--- src/scenes/Penguin_Eats_Fish.js
+++ src/scenes/Penguin_Eats_Fish.js
@@ -3,8 +3,17 @@ class Penguin_Eats_Fish extends Phaser.Scene {
         super("penguinEatsFishScene");
     }
 
+    preload() {
+        this.load.image('penguin_end', './assests/penguin_end.png');
+    }
+
     create() {
-        this.add.text(game.config.width / 2, game.config.height / 2, "you died from the penguin");
+        this.penguin_end = this.add.tileSprite(0, 0, 960, 720, 'penguin_end').setOrigin(0, 0);
+        let end_config = {
+            fontSize: 20,
+            align: 'center'
+        }
+        this.add.text(game.config.width / 15, game.config.height / 8, "You died from the penguin\nClick on the screen to restart", end_config);
     }
 
     update() {
