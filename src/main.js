e1f3bc282201611ea592ff169ab80f2dcc65d619
Marlene Inoue <mminoue@ucsc.edu>
2023-05-14 15:19:04 -0700

    added background musi

--- src/main.js
+++ src/main.js
@@ -7,8 +7,11 @@
 /**
  * Sources:
  *      mouse click: https://flaviocopes.com/phaser-mouse-input/
+ *      play background music: https://orangefreesounds.com/wave-piano-synth-loop/
  * 
  * Organization (15 points)
+ *  Submit a link to your GitHub repository that shows a history of multiple meaningful commits with descriptive messages (5)
+ *  Submit a playable link on GitHub pages (5)
  *  In main.js (or equivalent), include a comment header with your name, game title, approximate hours spent
  *  on project, and your creative tilt justification (see below) (5) 
  * 
@@ -17,7 +20,7 @@
  *      Menu, Play, Dolphin_Eats_Fish, Shark_Eats_Fish, Penguin_Eats_Fish
  *  Properly transition between Scenes and allow the player to restart w/out having to reload the page (5)
  *      mouse click to play screen
- *
+ *  Include in-game instructions using text or other means (e.g., tooltips, tutorial, diagram, etc.) (5)
  *  Have some form of player input/control appropriate to your game design (5)
  *      arrows to move
  *  Include one or more animated characters that use a texture atlas (5)
@@ -26,22 +29,24 @@
  *      background
  *  Implement proper collision detection (via Arcade Physics or a custom routine) (5)
  *      when fish touches dolphin, shark, or penguin (the front of those sprites)
- * 
- * 
+ *  Have looping background music (5)
+ *  -
  *  Use randomness to generate escalating challenge, e.g. terrain, pickups, etc. (5)
  *      dolphin, shark, and penguin spawns in different x or y axis
- * 
+ *  -
  *  Be theoretically endless (5)
- * 
+ *  Be playable for at least 15 seconds for a new player of low to moderate skill (5)
  *  Run without significant crashes or errors (5)
- *
+ *  -
  *  
  * Creative Tilt (10 points)
+ *  ...do something technically interesting? Are you particularly proud of a programming technique you implemented? Did you look beyond the class examples and learn how to do something new? (5)
+ *      While it wasn't a huge technical feat, it took me way too long to figure out how to do the mouse click, but I was pretty happy once I figured it out.
  * 
  *  ...have a great visual style? Does it use music or art that you're particularly proud of? Are you trying something new or clever with the endless runner form? (5)
  *      I have never drawn for games, especially an animated sprite. This was a bit of a challenge because I had to look up how 
- *      these animals looked in certain movements and since the sprites were originally drawn on a small axis, figuring out where
- *      to put the pixels for the body was hard. I think I did a really good job in the end; it's kind of easy to tell the order
+ *      these animals looked in certain movements and since the sprites were originally drawn on a small canvas, figuring out where
+ *      to put the pixels for the body was hard. I think I did a good job in the end; it's kind of easy to tell the order
  *      of the animals I drew since the animation looks cleaner at the end of some of them.
  *      I got the inspiration for my endless runner from the game Subway Surfer, where the player is being chased by the police.
  *      It's nearly the same idea because you need to avoid these bigger predators as a fish to survive
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -7,6 +7,9 @@ class Play extends Phaser.Scene {
         // load background image
         this.load.image('ocean_background', './assests/ocean_background.png');
 
+        // load music
+        this.load.audio('background_music', './assests/Wave-piano-synth-loop.wav');
+
         // load animals
         this.load.atlas('fish_anim', './assests/fish_anim.png', './assests/fish_anim.json');
         this.load.atlas('dolphin_anim', './assests/dolphin_anim.png', './assests/dolphin_anim.json');
@@ -18,6 +21,7 @@ class Play extends Phaser.Scene {
         // load background image
         this.ocean_background = this.add.tileSprite(0, 0, 960, 720, 'ocean_background').setOrigin(0, 0);
 
+        // create shark animation
         this.anims.create({
             key: 'shark',
             frames: this.anims.generateFrameNames('shark_anim', {prefix: 'shark ', end: 2, suffix: '.'}),
@@ -25,6 +29,7 @@ class Play extends Phaser.Scene {
             repeat: -1
         });
 
+        // create dolphin animation
         this.anims.create({
             key: 'dolphin',
             frames: this.anims.generateFrameNames('dolphin_anim', {prefix: 'dolphin ', end: 3, suffix: '.png'}),
@@ -32,6 +37,7 @@ class Play extends Phaser.Scene {
             repeat: -1
         });
 
+        // create penguin animation
         this.anims.create({
             key: 'penguin',
             frames: this.anims.generateFrameNames('penguin_anim', {prefix: 'penguin ', end: 3, suffix: ''}),
@@ -39,6 +45,7 @@ class Play extends Phaser.Scene {
             repeat: -1
         });
 
+        // create fish animation
         this.anims.create({
             key: 'fish',
             frames: this.anims.generateFrameNames('fish_anim', {prefix: 'fish', end: 2, suffix: ''}),
@@ -46,19 +53,22 @@ class Play extends Phaser.Scene {
             repeat: -1
         });
 
-
+        // create fish sprite
         this.fish = this.physics.add.sprite(game.config.width / 2, game.config.height / 2, 'fish_anim');
         this.fish.body.onOverlap = true;
         this.fish.setCollideWorldBounds(true);
 
+        // create shark sprite
         this.shark = this.physics.add.sprite(game.config.width + 32, Phaser.Math.Between(0 + 64, game.config.height - 64), 'shark_anim');
         this.shark.body.onOverlap = true;
         this.shark.setScale(4);
 
+        // create dolphin sprite
         this.dolphin = this.physics.add.sprite(game.config.width + 42, Phaser.Math.Between(0 + 64, game.config.height - 64), 'dolphin_anim');
         this.dolphin.body.onOverlap = true;
         this.dolphin.setScale(3);
 
+        // create penguin sprite
         this.penguin = this.physics.add.sprite(Phaser.Math.Between(0, game.config.width), 0, 'penguin_anim');
         this.penguin.body.onOverlap = true;
         this.penguin.setScale(2);
@@ -70,6 +80,9 @@ class Play extends Phaser.Scene {
         this.penguin.play('penguin');
 
         cursor = this.input.keyboard.createCursorKeys();
+
+        this.song = this.sound.add('background_music', {loop: true, volume: 0.3});
+        this.song.play();
     }
 
     update() {
@@ -152,7 +165,8 @@ class Play extends Phaser.Scene {
             fish.x < penguin.x &&
             fish.y > penguin.y - fish.width && 
             fish.y < penguin.y + fish.width) {
-            this.scene.start('penguinEatsFishScene');
+                this.song.stop();
+                this.scene.start('penguinEatsFishScene');
         }
     }
 
@@ -161,7 +175,8 @@ class Play extends Phaser.Scene {
             fish.x < shark.x - shark.width &&
             fish.y > shark.y - shark.height && 
             fish.y < shark.y + (1.5 * shark.height)) {
-            this.scene.start('sharkEatsFishScene');
+                this.song.stop();
+                this.scene.start('sharkEatsFishScene');
         }
     }
 
@@ -170,7 +185,8 @@ class Play extends Phaser.Scene {
             fish.x < dolphin.x - dolphin.width &&
             fish.y > dolphin.y - dolphin.height && 
             fish.y < dolphin.y + dolphin.height) {
-            this.scene.start('dolphinEatsFishScene');
+                this.song.stop();
+                this.scene.start('dolphinEatsFishScene');
         }
     }
 }
